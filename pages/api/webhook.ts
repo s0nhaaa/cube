2bb180f3430d3062b86bbc5dc3928abad05c0fbd
sonhaaa <nguyensonha.hanz@gmail.com>
2023-05-21 04:44:22 +0700

    feat: payment webhook

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -5,19 +5,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const headers = req.headers
   const payload = req.body
 
-  // try {
-  //   await verifyWebhookSignature({
-  //     payload: JSON.stringify(payload),
-  //     headers: headers as Record<string, string>,
-  //     webhook_secret: process.env.WEBHOOK_SECRET!,
-  //   })
-  // } catch (err) {
-  //   res.status(400).json({
-  //     message: 'Invalid webhook signature',
-  //   })
-  // }
+  try {
+    await verifyWebhookSignature({
+      payload: JSON.stringify(payload),
+      headers: headers as Record<string, string>,
+      webhook_secret: process.env.WEBHOOK_SECRET!,
+    })
+  } catch (err) {
+    res.status(400).json({
+      message: 'Invalid webhook signature',
+    })
+  }
 
   res.status(200).json({
     message: 'hello iam function',
+    payload,
   })
 }
